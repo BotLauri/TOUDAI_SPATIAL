5bf1e4fae7d027dfcd59b3741c2705295d917eb2
BotLauri <jonas7654@live.se>
2022-11-23 16:20:13 +0900

    Started the project.
    Still a lot of work to do...

---
+++ spatial_project.R
@@ -0,0 +1,7 @@
+# Spatial project about the movement of people in Sweden 
+# based on the learning institutions in that particular area.
+setwd("~/GitHub/TOUDAI_SPATIAL")
+data <- read.csv(file = "nice_data.csv")
+head(data)
+
+
